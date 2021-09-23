f730057e7441fabda9898fad1e29c2eb71c94377
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-09-23 10:54:34 +0300

    modified main/urls.py

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from .views import home_page,signup_view,post_project,project_view, search_resul
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views  as auth_views
+from . import views
 
 
 app_name ="main"
@@ -10,18 +11,19 @@ app_name ="main"
 
 urlpatterns=[
  
-path('',home_page,name='home'),
-path('signup/', signup_view, name='signup'),
-path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+path('home/', views.home_page,name="home"),
+path('accounts/', include('django.contrib.auth.urls')),
+path('signup/', views.signup_view, name='signup'),
+path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 path('logout/', auth_views.LogoutView.as_view(next_page = '/')),
 
-path('post_project/', post_project,name="post_project"),
-path('project_view/<int:id>', project_view,name="project_view"),
-path('search/',search_results, name='search_results'),
+path('post_project/', views.post_project,name="post_project"),
+path('project_view/<int:id>', views.project_view,name="project_view"),
+path('search/', views.search_results, name='search_results'),
 
-path('user_profile/<str:username>',user_profile, name='user_profile'),
-path('profile/<str:username>',profile, name='profile'),
-path('update_profile/<str:username>',update_profile, name='update_profile'),
+path('user_profile/<str:username>', views.user_profile, name='user_profile'),
+path('profile/<str:username>', views.profile, name='profile'),
+path('update_profile/<str:username>', views.update_profile, name='update_profile'),
 ]
 
 if settings.DEBUG:
